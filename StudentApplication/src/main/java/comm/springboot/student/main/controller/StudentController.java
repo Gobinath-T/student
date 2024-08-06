4091d7649d8a539c3f95562a6387d4fb279c8676
gobinath <gobinath060999@gmail.com>
2024-08-06 10:10:09 +0530

    first test commit

---
+++ StudentApplication/src/main/java/comm/springboot/student/main/controller/StudentController.java
@@ -0,0 +1,25 @@
+package comm.springboot.student.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+	
+	
+	@GetMapping("/students")
+	public String studentList() {
+		return null;
+	}
+	
+	@GetMapping("/student/{id}")
+	public String getStudentById(@PathVariable int id){
+		return null;
+	}
+	
+	
+
+}
