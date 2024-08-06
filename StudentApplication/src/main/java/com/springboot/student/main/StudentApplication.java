99cfe103f38e41aac558b4c688a78ac98dbcff9f
gobinath <gobinath060999@gmail.com>
2024-08-06 21:04:37 +0530

    Docker Commit

--- StudentApplication/src/main/java/com/springboot/student/main/StudentApplication.java
+++ StudentApplication/src/main/java/com/springboot/student/main/StudentApplication.java
@@ -1,9 +1,11 @@
 package com.springboot.student.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SpringBootConfiguration
 public class StudentApplication {
 
 	public static void main(String[] args) {
